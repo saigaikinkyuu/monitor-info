190110161412e13dd1574c34889eafad7de5439b
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-11-12 15:19:19 +0900

    Update text-scroll.js

--- js/text-scroll.js
+++ js/text-scroll.js
@@ -6,7 +6,7 @@ marqueeTexts.forEach((marqueeText) => {
       translate: [0, 'calc(-100% - 1rem)']
     },
     {
-      duration: 50000,
+      duration: 10000,
       iterations: Infinity
     }
   );
