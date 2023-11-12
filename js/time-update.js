9ef22f51f1f40753af1a051dc293693a374febd0
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-11-12 16:41:24 +0900

    Create time-update.js

---
+++ js/time-update.js
@@ -0,0 +1,12 @@
+function timeUpdate() {
+  var now_time = new Date();
+  var year = now_time.getFullYear();
+  var month = now_time.getMonth()+1;
+  var date = now_time.getDate();
+  var hours = now_time.getHours();
+  var minute = now_time.getMinutes();
+  var second = now_time.getSeconds();
+  var time = year + "/" + month + "/" + date + " " + hours + ":" + minute + ":" + second;
+  document.getElementById('now_time').textContent = time;
+  setInterval(timeUpdate, 100);
+}
