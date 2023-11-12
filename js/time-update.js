aceda1303f9ab5e675accc45c2715f3401b56b07
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-11-12 17:17:42 +0900

    Update time-update.js

--- js/time-update.js
+++ js/time-update.js
@@ -1,6 +1,6 @@
 function update() {
   var element = document.getElementById("now_time");
-  element.textContent = new Date().toLocaleTimeString(); // 現在時刻を表示
+  element.textContent = new Date(); // 現在時刻を表示
 
   // 次の更新処理を予約
   setTimeout(update, 1000); // 1秒後に再度実行
