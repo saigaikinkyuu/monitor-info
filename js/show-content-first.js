b37c71055755c026bd60f9041d48caabd529a59b
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-11-12 17:53:58 +0900

    Delete js/show-content-first.js

--- js/show-content-first.js
+++
@@ -1,8 +0,0 @@
-// ページが読み込まれた時点から10秒後に要素を表示
-  document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-      // 要素の表示スタイルを変更
-      document.getElementById('show-cont1').style.display = 'none';
-      document.getElementById('slideshow-container').style.display = 'block';
-    }, 10000); // 10000ミリ秒 = 10秒
-    });
