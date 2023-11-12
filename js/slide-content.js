df95c2d128c23728537066836f0ee09ba35e1226
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-11-12 14:24:31 +0900

    Create slide-content.js

---
+++ js/slide-content.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+   const slides = document.querySelectorAll('.slide');
+   let currentSlideIndex = 0;
+
+   function showSlide(index) {
+     slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+     });
+   }
+
+   function nextSlide() {
+     currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+     showSlide(currentSlideIndex);
+  }
+
+  // 最初の表示
+  showSlide(currentSlideIndex);
+
+ // 5秒ごとにスライドを変更
+  setInterval(nextSlide, 5000);
+});
