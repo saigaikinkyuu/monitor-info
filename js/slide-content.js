96fc919cdf7fcf8fa567b9f304887415502aab73
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-12-02 18:07:12 +0900

    Update slide-content.js

--- js/slide-content.js
+++ js/slide-content.js
@@ -18,4 +18,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // 5秒ごとにスライドを変更
     setInterval(nextSlide, 15000);
-  });
+});
+
+function keyActionE() {
+    document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlideIndex = 1;
+
+    function showSlide(index) {
+      slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+      });
+    }
+
+    function nextSlide() {
+      currentSlideIndex = 1 % slides.length;
+      showSlide(currentSlideIndex);
+    }
+
+    // 最初の表示
+    showSlide(currentSlideIndex);
+
+    // 5秒ごとにスライドを変更
+    setInterval(nextSlide, 15000);
+    });
+}
